c0286d7b8393124b1f7ab9d09efb9c298964a0d3
Sean R Reid <sean@torchcodelab.com>
2024-03-03 23:46:49 -0500

    Forgot secret key

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Settings:
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{
         POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    SECRET_KEY: str = 'Stay Rad'
 
 
 settings = Settings()
